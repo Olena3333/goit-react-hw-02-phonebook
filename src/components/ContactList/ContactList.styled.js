e4c10018572325697ebb8df94e7c9f2fd9154ad8
Olena <helensmart2323@gmail.com>
2023-10-25 00:02:56 +0200

    task2

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledList = styled.ul`
+  list-style: none;
+  padding-left: 0;
+
+  li {
+    font-size: 20px;
+    margin: 15px;
+    display: flex;
+    justify-content: space-between;
+  }
+  button {
+    margin-left: 30px;
+  }
+`;
