234bf26a07e9dc093df4805f74664e52af55e810
Olena <helensmart2323@gmail.com>
2023-10-25 00:05:52 +0200

    2

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 export const StyledList = styled.ul`
   list-style: none;
   padding-left: 0;
+  margin-top: 40px;
 
   li {
     font-size: 20px;
-    margin: 15px;
+    margin: 15px 0px;
     display: flex;
     justify-content: space-between;
   }
